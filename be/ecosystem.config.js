38ef514a08cfe16f6b58c36fb6b0c746d376e4c9
Nopallse <2211522020_naufal@student.unand.ac.id>
2025-11-09 15:31:40 +0700

    s

---
+++ be/ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps: [
+    {
+      name: 'hexa-api',
+      script: './src/server.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+      env_dev: {
+        NODE_ENV: 'development',
+        PORT: 3000,
+        MIDTRANS_USE_SANDBOX: 'true',
+      },
+      error_file: './logs/pm2-error.log',
+      out_file: './logs/pm2-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: '10s',
+    },
+  ],
+};
+
